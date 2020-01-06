877518907837c7ad199e58f05de3c8b43e68de56
sankeerthp <poojalasankeerth@gmail.com>
2020-01-06 17:22:35 +0530

    addition feature

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
-from flask import Flask, request
+#!/usr/bin/env python
+from flask import Flask, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return 'Usage;\n<Operation>?A=<Value1>&B=<Value2>\n'
-
+	if request.method == 'GET':
+        #html form
+          return '''
+            <form method="post">
+                First number: <input type="text" name="A" />
+                Second number: <input type="text" name="B" />
+                <p><input type="submit" name="operator" value="Add" />
+	    </form>
+        '''
+	elif request.method == 'POST':
+         a = request.form.get('operator')
+         if a == 'Add':
+            A = request.form.get('A')
+            B = request.form.get('B')
+            return redirect(url_for('add', A=A, B=B))
 
 @app.route('/add')
-def addition():
-    value1=request.args.get('A',default = 0, type = int)
-    value2=request.args.get('B',default = 0, type = int)
-    result=value1+value2
-    return '%d \n' % result
+def add():
+    dict = request.args.to_dict()
+    A = eval(dict['A'])
+    B = eval(dict['B'])
+    result = A+B
+    return 'Result is: %s' % result
 
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
