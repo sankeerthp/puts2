3573f45e44e469ad386b93f92a17062d4dddd0fc
sankeerthp <55428295+sankeerthp@users.noreply.github.com>
2020-01-06 13:43:09 +0100

    Create test.py
    Test script for multiplication

---
+++ test.py
@@ -0,0 +1,26 @@
+import unittest 
+import requests
+import json 
+from fractions import Fraction 
+
+number1 = [2,21,5,8]
+number2 = [7,11,3,4]
+
+mul_main = []
+mul_test = []
+
+for i in range(0,len(number1)):
+        parameters={"A":Fraction(number1[i]),"B":Fraction(number2[i])}
+
+        test_m = number1[i] * number2[i]
+        mul_test.append(round(test_m,3))
+
+        url_m = 'http://127.0.0.1:5000/mul'
+        r3 = requests.get(url_m, params=parameters)
+        data3 = r3.json()
+        mul_main.append(round(data3,3))
+
+        if mul_main[i] == mul_test[i]:
+                print("multiplication successfull:OK")
+        else:
+                print("multiplication failed")
