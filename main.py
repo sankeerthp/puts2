4937485e2cd5e8c0f8ad5a335c410c310ad626ac
sankeerthp <poojalasankeerth@gmail.com>
2020-01-06 17:36:09 +0530

    division feature

--- main.py
+++ main.py
@@ -11,22 +11,22 @@ def index():
             <form method="post">
                 First number: <input type="text" name="A" />
                 Second number: <input type="text" name="B" />
-                <p><input type="submit" name="operator" value="Subtract" />
+                <p><input type="submit" name="operator" value="Divide" />
 	    </form>
         '''
 	elif request.method == 'POST':
          a = request.form.get('operator')
-         if a == 'Subtract':
+         if a == 'Divide':
             A = request.form.get('A')
             B = request.form.get('B')
-            return redirect(url_for('sub', A=A, B=B))
+            return redirect(url_for('div', A=A, B=B))
 
-@app.route('/sub')
-def add():
+@app.route('/div')
+def div():
     dict = request.args.to_dict()
     A = eval(dict['A'])
     B = eval(dict['B'])
-    result = A-B
+    result = A/B
     return 'Result is: %s' % result
 
 
